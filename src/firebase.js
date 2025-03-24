54ce61645f1b6b3e6e9cf8bf82c1877a8c446472
bwx99 <elieboueiri@gmail.com>
2025-03-24 01:20:46 -0600

    Fix: Firebase config with valid API key

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore, collection, getDocs, setDoc, doc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDPoBiZQK1NaQjcEb6_FqcE7c7nvZJA2PE",
@@ -12,22 +12,13 @@ const firebaseConfig = {
   appId: "1:481080450835:web:e4d19cf5ef26d1d9bde0e6"
 };
 
-
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 
-export const signIn = () => signInWithPopup(auth, provider);
+export const signIn = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider);
+};
+
 export const signOutUser = () => signOut(auth);
-
-export const loadGoals = async (uid) => {
-  const docRef = doc(db, 'goals', uid);
-  const docSnap = await getDoc(docRef);
-  return docSnap.exists() ? docSnap.data().goals : [];
-};
-
-export const saveGoals = async (uid, goals) => {
-  await setDoc(doc(db, 'goals', uid), { goals });
-};
